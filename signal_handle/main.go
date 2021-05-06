cfc7083e2511229ed76713a0fa830f132505baca
younghyun.ahn <younghyun.ahn@gmail.com>
2021-05-06 20:28:41 +0900

    singal handle example

---
+++ signal_handle/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	//signal.Notify(sigs, os.Interrupt)
+	signal.Notify(sigs, os.Kill)
+
+	go func() {
+		sig := <-sigs
+		fmt.Println(sig)
+		done <- true
+	}()
+
+	fmt.Println("awaiting signal")
+	<-done
+	fmt.Println("existing")
+}
